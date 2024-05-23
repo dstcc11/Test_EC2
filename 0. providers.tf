433d706e0ebb0472f501304fa5d2bb4a53481c8b
jf3079 <jerome3079@gmail.com>
2024-05-23 11:44:03 -0400

    UUU

--- 0. providers.tf
+++ 0. providers.tf
@@ -10,3 +10,17 @@ terraform {
     dynamodb_table = "test-tf-aws"
   }
 }
+
+data "terraform_remote_state" "backend" {
+  backend = "s3"
+  config = {
+    bucket         = "test-tf-aws"
+    key            = "terraform.tfstate" #name of the S3 object that will store the state file
+    region         = "us-east-1"
+    dynamodb_table = "test-tf-aws"
+  }
+}
+
+output "tf_backend" {
+  value = data.terraform_remote_state.backend
+}
