0c634028713de156266d633f697a0af4f0f5d5d0
jf3079 <jerome3079@gmail.com>
2024-05-23 11:12:06 -0400

    udpated

--- 0. providers.tf
+++ 0. providers.tf
@@ -3,11 +3,10 @@ provider "aws" {
 }
 
 terraform {
-  cloud {
-    organization = "KuTest"
-
-    workspaces {
-      name = "Test_EC2"
-    }
+  backend "s3" {
+    bucket         = "test-tf-aws"
+    key            = "terraform.tfstate" #name of the S3 object that will store the state file
+    region         = "us-east-1"
+    dynamodb_table = "test-tf-aws"
   }
 }
