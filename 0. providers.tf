a9b2499ee816fd315642f9aed13f31589f3c60f3
jf3079 <jerome3079@gmail.com>
2024-05-23 13:29:22 -0400

    l

--- 0. providers.tf
+++ 0. providers.tf
@@ -15,6 +15,16 @@ terraform {
 }
 
 terraform {
+  backend "s3" {
+    bucket         = "test-tf-aws"
+    key            = "terraform.tfstate" #name of the S3 object that will store the state file
+    region         = "us-east-1"
+    dynamodb_table = "test-tf-aws"
+  }
+}
+*/
+
+terraform {
   cloud {
     organization = "KuTest"
 
@@ -23,15 +33,3 @@ terraform {
     }
   }
 }
-*/
-
-terraform {
-  backend "s3" {
-    bucket         = "test-tf-aws"
-    key            = "terraform.tfstate" #name of the S3 object that will store the state file
-    region         = "us-east-1"
-    dynamodb_table = "test-tf-aws"
-  }
-}
-
-
