74f10598ab292eb27e2d4482123306ab46c76524
jf3079 <jerome3079@gmail.com>
2024-05-23 11:28:11 -0400

    modified vol

--- 1. locals.tf
+++ 1. locals.tf
@@ -16,13 +16,7 @@ locals {
       instance_type = "t2.micro"
       ami           = data.aws_ami.latest_amz_windows2019srv.id
       ebs_volumes = {
-        "vol1" = {
-          device_name = "/dev/sdh"
-          size        = "50"
-          type        = "io2"
-          iops        = "5000"
-        }
-        "vol2" = {
+        "vola" = {
           device_name = "/dev/sdm"
           size        = "75"
           type        = "gp3"
