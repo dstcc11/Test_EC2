218a43a3701a8ecdbb9acbfc3dd2b6e86d693d78
jf3079 <jerome3079@gmail.com>
2024-06-18 11:43:12 -0400

    udpated

--- 4. iam.tf
+++ 4. iam.tf
@@ -1,3 +1,54 @@
+# ------------------------------- IAM for Barracuda --------------------------------------------
+
+resource "aws_iam_policy" "multi-ip" {
+  name   = "${var.site}-multi-ip"
+  path   = "/"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:UnassignPrivateIpAddresses",
+                "ec2:AssignPrivateIpAddresses"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "multi-ip" {
+  path = "/"
+  name = "${var.site}-multi-ip"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "multi-ip" {
+  policy_arn = aws_iam_policy.multi-ip.arn
+  role       = aws_iam_role.multi-ip.name
+}
+
+resource "aws_iam_instance_profile" "multi-ip" {
+  name = "${var.site}-multi-ip-iam-instance-profile"
+  role = aws_iam_role.multi-ip.name
+}
+
 # ------------------------------- IAM for ha --------------------------------------------
 
 resource "aws_iam_policy" "ha" {
