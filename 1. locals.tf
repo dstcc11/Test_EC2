7df4994e894bf760743f9be96a33ff45f8104825
jf3079 <jerome3079@gmail.com>
2024-08-21 14:36:11 -0400

    udpated

--- 1. locals.tf
+++ 1. locals.tf
@@ -4,5 +4,9 @@ locals {
       instance_type = "t2.micro"
       ami           = data.aws_ami.latest_ubuntu.id
     }
+     "test-3" = {
+      instance_type = "t2.micro"
+      ami           = data.aws_ami.latest_ubuntu.id
+    }
   }
 }
