cbb29b40a7d84ff15f5890cf1f93577d6a15de6b
jf3079 <jerome3079@gmail.com>
2024-08-21 14:41:24 -0400

    dujpated

--- 1. locals.tf
+++ 1. locals.tf
@@ -4,11 +4,7 @@ locals {
       instance_type = "t2.micro"
       ami           = data.aws_ami.latest_ubuntu.id
     }
-"test-2" = {
-      instance_type = "t2.micro"
-      ami           = data.aws_ami.latest_ubuntu.id
-    }
-     "test-3" = {
+    "test-2" = {
       instance_type = "t2.micro"
       ami           = data.aws_ami.latest_ubuntu.id
     }
