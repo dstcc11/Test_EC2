7749d0200e760e054207a4e9a141d6b36e18407c
jf3079 <jerome3079@gmail.com>
2024-09-11 10:50:00 -0400

    updated

--- 1. locals.tf
+++ 1. locals.tf
@@ -3,6 +3,13 @@ locals {
     "test1" = {
       instance_type = "t2.micro"
       ami           = data.aws_ami.latest_ubuntu.id
+      ebs_volumes = {
+        "MountPoints" = {
+          device_name = "/dev/sdd"
+          size        = "1"
+          type        = "gp3"
+        }
+      }
       tags = {
         "t1" = "a1"
         "t2" = "a2"
